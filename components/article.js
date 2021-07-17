76b3a9ee5d925a92b45909e8d577864bb7d8c0bf
william-ty <william.trahay@gmail.com>
2021-07-17 13:48:05 +0200

    [UPDATE] components and article added

---
+++ components/article.js
@@ -0,0 +1,22 @@
+export default function Article() {
+  return (
+    <div className="max-w-sm w-full lg:max-w-full lg:flex">
+      <div className="m-10 border rounded-md border-gray-400 p-4 flex flex-col justify-between leading-normal">
+          <div className="mb-8">
+            <div className="text-gray-900 font-bold text-xl mb-2">
+                Title A
+            </div>
+            <p className="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam aut adipisci rerum molestiae aliquam expedita consectetur nemo tempore non eveniet, recusandae blanditiis deserunt perferendis itaque facere dicta consequuntur? Saepe, ullam pariatur nihil fugit tempore quas earum aliquid reprehenderit maxime, doloremque aut, labore fuga nemo cupiditate? Fuga similique facilis commodi, beatae illum aut quasi necessitatibus nihil inventore ad dolorem maxime saepe esse cupiditate consequatur corporis libero autem excepturi labore doloremque cumque assumenda magnam ipsum? Ipsam alias quia accusantium et temporibus enim voluptates ab! Dolorum porro maiores esse in cum perferendis commodi dolor asperiores, velit ab modi corporis expedita distinctio quibusdam. Voluptatum nihil accusantium aliquid quam itaque consequatur minus, provident natus totam rerum, sint cumque aperiam non explicabo laudantium omnis iure asperiores!
+            </p>
+          </div>
+          <div className="flex items-center">
+            <div className="text-sm">
+                <p className="text-gray-900 leading-none">Anonymous</p>
+                <p className="text-gray-600">July 17</p>
+            </div>
+          </div>
+      </div>
+  </div>
+  )
+}
